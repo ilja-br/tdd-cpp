e5274bf39c8f607e976f92161bb77d52a1baebca
Ilja Bratchikov <ilja.bratchikov@gmail.com>
2017-06-06 12:27:01 +0200

    Fixed CMakeLists compiler setting; minimal functionality of soundex is implemented

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+class Soundex {
+public:
+    std::string encode(const std::string& word) const {
+        return "A";
+    }
+};
+
 TEST(SanityCheck, FrameworkWorks) {
     ASSERT_EQ(1, 1);
+}
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    Soundex soundex;
+
+    auto encoded = soundex.encode("A");
+    ASSERT_EQ(encoded, "A");
 }
 
 int main(int argc, char** argv) {
